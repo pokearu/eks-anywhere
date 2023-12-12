4163c01956927ffd79a87d45de977de5ff3ea91d
Aravind Ramalingam <ramaliar@amazon.com>
2023-12-12 15:44:37 -0800

    adding commands to nodelogs command

--- cmd/eks-a-tool/cmd/nodelogs.go
+++ cmd/eks-a-tool/cmd/nodelogs.go
@@ -17,6 +17,66 @@ type nodeLogsOptions struct {
 	privateKeyPath string
 	username       string
 	nodeIps        []string
+	osFamily       string
+}
+
+type nodeLogCommand struct {
+	resourceName string
+	command      string
+}
+
+func NewNodeLogCommands(osFamily string) []nodeLogCommand {
+	if osFamily == "bottlerocket" {
+		return []nodeLogCommand{
+			{
+				resourceName: "kubelet",
+				command:      "sudo sheltie journalctl -u kubelet.service --no-pager",
+			},
+			{
+				resourceName: "containerd",
+				command:      "sudo sheltie journalctl -u containerd.service --no-pager",
+			},
+			{
+				resourceName: "etcd",
+				command:      "sudo sheltie cat /var/log/containers/$(sudo sheltie ls /var/log/containers/ | grep etcd)",
+			},
+			{
+				resourceName: "cloud-init",
+				command:      "sudo sheltie journalctl _COMM=host-ctr --no-pager",
+			},
+			{
+				resourceName: "manifests",
+				command:      "sudo sheltie cat /etc/kubernetes/manifests/*",
+			},
+		}
+	}
+
+	return []nodeLogCommand{
+		{
+			resourceName: "kubelet",
+			command:      "sudo journalctl -u kubelet.service --no-pager",
+		},
+		{
+			resourceName: "containerd",
+			command:      "sudo journalctl -u containerd.service --no-pager",
+		},
+		{
+			resourceName: "etcd-unstacked",
+			command:      "sudo journalctl -u etcd.service --no-pager",
+		},
+		{
+			resourceName: "etcd-stacked",
+			command:      "sudo crictl logs $(sudo crictl ps --name etcd -q)",
+		},
+		{
+			resourceName: "cloud-init",
+			command:      "sudo cat /var/log/cloud-init-output.log",
+		},
+		{
+			resourceName: "manifests",
+			command:      "sudo cat /etc/kubernetes/manifests/*",
+		},
+	}
 }
 
 const (
@@ -43,6 +103,7 @@ func init() {
 	nodeLogsCmd.Flags().StringVarP(&nodeLogsOpt.privateKeyPath, "privatekey", "k", "", "Private key file path")
 	nodeLogsCmd.Flags().StringVarP(&nodeLogsOpt.username, "username", "u", "", "Username to access the nodes")
 	nodeLogsCmd.Flags().StringSliceVarP(&nodeLogsOpt.nodeIps, "nodeip", "i", make([]string, 0), "IP addresses of the nodes")
+	nodeLogsCmd.Flags().StringVarP(&nodeLogsOpt.osFamily, "osfamily", "o", "", "OS family of the nodes (bottlerocket/ubuntu/rhel)")
 
 	_ = nodeLogsCmd.MarkFlagRequired("privatekey")
 }
@@ -50,13 +111,17 @@ func init() {
 func nodeLogs(ctx context.Context) error {
 	writer, _ := filewriter.NewWriter("nodelogs")
 	ssh := executables.NewLocalExecutablesBuilder().BuildSSHExecutable()
+	nodeLogCommands := NewNodeLogCommands(nodeLogsOpt.osFamily)
+
 	for _, nodeIP := range nodeLogsOpt.nodeIps {
-		stdout, err := ssh.RunCommand(ctx, nodeLogsOpt.privateKeyPath, nodeLogsOpt.username, nodeIP, "sudo sheltie apiclient get settings.network.hostname")
-		if err != nil {
-			return err
-		}
-		if err := writeNodeLogsToFile(writer, stdout, nodeIP, "logName"); err != nil {
-			return fmt.Errorf("writing logs to file: %v", err)
+		for _, nlc := range nodeLogCommands {
+			stdout, err := ssh.RunCommand(ctx, nodeLogsOpt.privateKeyPath, nodeLogsOpt.username, nodeIP, nlc.command)
+			if err != nil {
+				stdout = *bytes.NewBufferString(err.Error())
+			}
+			if err := writeNodeLogsToFile(writer, stdout, nodeIP, nlc.resourceName); err != nil {
+				return fmt.Errorf("writing logs to file: %v", err)
+			}
 		}
 	}
 
